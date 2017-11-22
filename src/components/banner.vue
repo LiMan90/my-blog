a9881e948059d74c33dc4a3676f18f82d45a8cba
liman <manlil@isoftstone.com>
2017-11-22 17:48:03 +0800

    首页完成

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -3,40 +3,14 @@
     <div class="banner" v-for="banner in list">
         <div class="overhidden">
           <a :href="banner.id">
-            <img :src="banner.image">
+            <img class="post-image" :src="banner.image">
           </a>
         </div>
         <h3><a :href="banner.id" :title="banner.title">{{banner.title}}</a></h3>
         <div><p>{{banner.contentAbstract}}</p></div>
     </div>
 
-    <!--    <div class="banner">
-          <banner>
-            <a href="/">
-              <img src="/static/card.jpg">
-            </a>
-            <h3><a href="#">标题标题标题</a></h3>
-            <div><p>内容缩略内容缩略内容缩容缩内容缩略内容缩略内容缩容缩</p></div>
-          </banner>
-        </div>
-        <div class="banner">
-          <banner>
-            <a href="/">
-              <img src="/static/card.jpg">
-            </a>
-            <h3><a href="#">标题标题标题</a></h3>
-            <div><p>内容缩略内容缩略内容缩容缩内容缩略内容缩略内容缩容缩内容缩略内容缩略内容缩容缩</p></div>
-          </banner>
-        </div>
-        <div class="banner">
-          <banner>
-            <a href="/">
-              <img src="/static/card.jpg">
-            </a>
-            <h3><a href="#">标题标题标题</a></h3>
-            <div><p>内容缩略内容缩略内容缩容缩内容缩略内容缩略内容缩容缩内容缩略内容缩略内容缩容缩</p></div>
-          </banner>
-        </div>-->
+
 
   </div>
 
