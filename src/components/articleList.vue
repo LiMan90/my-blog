7fff85a9f53748ea2569746714c4f5f6526ddf01
liman <manlil@isoftstone.com>
2017-12-04 18:00:24 +0800

    分类查找

--- src/components/articleList.vue
+++ src/components/articleList.vue
@@ -11,7 +11,7 @@
       <div class="entry-meta"> 作者:
         <router-link class="entry-title" to="/about"><span>{{item.author}}</span></router-link>
         <span> 发布时间:{{item.createTime}}</span>
-        <span> 阅读次数: 105 </span> <span> 文章分类: <a href="javascript:;" @click="category(item.categoryId)">{{item.category}}</a> </span>
+        <span> 阅读次数: 105 </span> <span class="entry-title"> 文章分类: <a href="javascript:;" @click="category(item.categoryId)">{{item.category}}</a> </span>
       </div>
 
       <div class="entry-content description clearfix">
